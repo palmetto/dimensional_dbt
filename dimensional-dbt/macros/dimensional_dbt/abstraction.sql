cf60fd78a232593f5ef34657fd780425be9427c4
Ethan Knox <ethan.knox@palmetto.com>
2021-07-16 14:10:34 -0400

    dimensional dbt is now abstracted

--- dimensional-dbt/macros/dimensional_dbt/abstraction.sql
+++ dimensional-dbt/macros/dimensional_dbt/abstraction.sql
@@ -25,3 +25,15 @@ ORDER BY
     {{ caller() }}
     {{ dimensional_dbt.after_select(source_ctes, column_count) }}
 {%- endmacro -%}
+
+
+{%- macro source_builder(source_ref, unique_identifier, alias=none) -%}
+    {%- set cte_name = alias if alias else source_ref.name -%}
+    {{cte_name}} AS (
+        SELECT
+            *
+            ,{{unique_identifier}} AS dimensional_dbt_unique_identifier
+        FROM 
+            {{ ref(source_ref) }}
+    )
+{%- endmacro -%}
--- test-dimensional-dbt/models/dim_product_fully_abstracted.sql
+++ test-dimensional-dbt/models/dim_product_fully_abstracted.sql
@@ -1,35 +1,14 @@
 WITH
 
-stripe AS (
-    SELECT
-        *
-        ,chiquita_product_id::NUMBER AS dimensional_dbt_unique_identifier
-    FROM
-        {{ ref('_stripe_source') }}
+{{ dimensional_dbt.source_builder('_stripe_source','chiquita_product_id::NUMBER', 'stripe') }}
+,{{ dimensional_dbt.source_builder('_warehouse_source',"REPLACE(vendor_id,'chi-','')::NUMBER", 'warehouse') }}
+,{{ dimensional_dbt.source_builder('_vendor_source','id::NUMBER', 'vendor') }}
 
-)
-, warehouse AS (
-    SELECT
-        *
-        ,REPLACE(vendor_id,'chi-','')::NUMBER AS dimensional_dbt_unique_identifier
-    FROM
-        {{ ref('_warehouse_source') }}
-)
-, vendor AS (
-    SELECT
-        *
-        ,id::NUMBER AS dimensional_dbt_unique_identifier
-    FROM
-        {{ ref('_vendor_source') }}
-)
-{%- set source_ctes=['stripe','warehouse','vendor'] -%}
-
-{% call dimensional_dbt.column_selection(source_ctes, 5) %}
+{% call dimensional_dbt.column_selection(['stripe','warehouse','vendor'], 5) %}
     SELECT 
         stripe_d.name AS product_name
         ,stripe_d.retail_price AS retail_price
         ,vendor_d.wholesale_cost AS wholesale_cost
         ,stripe_d.description AS description
         ,IFNULL(warehouse_d.storage_type,'Storage Unkown') AS required_type_of_storage_in_warehouse
-
 {% endcall %}
